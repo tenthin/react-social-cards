2e7d7e52184f1a410be14444486ee10660be786d
Tenzin <tenthinten@gmail.com>
2022-02-12 16:00:59 +0530

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect (() => {
+    (async () => {
+      let userData;
+      try{
+        const response = await fetch('https://randomuser.me/api/?results=10');
+        const userData = await response.json().results;
+      } catch (error) {
+        console.log(error);
+        userData = [];
+      }
+
+      setUsers(userData);
+    })();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
