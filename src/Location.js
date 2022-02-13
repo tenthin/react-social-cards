f238d8b3ccef70d2f17b811c624c37fb165b6244
Tenzin <tenthinten@gmail.com>
2022-02-13 14:44:46 +0530

    add locaiton

---
+++ src/Location.js
@@ -0,0 +1,13 @@
+const Location = ({location}) => {
+    return (
+        <div className="location">
+            <p>{location.street.number},{location.street.name}</p>
+            <p>{location.city}</p>
+            <p>{location.state}</p>
+            <p>{location.postcode}</p>
+            <p>{location.country}</p>
+        </div>
+    )
+}
+
+export default Location;
--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,9 +1,13 @@
-import "./SocialCard.css"
+import "./SocialCard.css";
+import Location from "./Location";
+
+
 const SocialCard = ({ userData }) => {
     return (
         <div className="card">
             <div className="card__title">{userData.name.first} {userData.name.last}</div>
             <div className="card__body">
+                <Location location={userData.location}/>
                 <div className="card__image"><img src={userData.picture.medium}/></div>
             </div>
         </div>
